4172017fe0bdaea4c02f2f652ffbf616dd605992
othmanoach <othmanoach@gmail.com>
2024-04-22 07:52:55 +0100

    async functions

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Importing the wait_random from the previous python file that i’ve written and
+    write an async routine called wait_n. """
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ execute multiple coroutines at the same time with async  """
+    delays: List[float] = []
+    all_delays: List[float] = []
+    for i in range(n):
+        delays.append(wait_random(max_delay))
+    for delay in asyncio.as_completed(delays):
+        earliest_result = await delay
+        all_delays.append(earliest_result)
+    return all_delays
